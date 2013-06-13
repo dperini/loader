2712b941bc5d56732ab596fc9e9e46d185277f75
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2013-06-13 12:19:39 -0700

    Update loadJSON.js
    please verify inline comments, marked with `[ag]` prefix

--- src/loadJSON.js
+++ src/loadJSON.js
@@ -16,7 +16,8 @@ function loadJSON(url, callback) {
   html, iframe, jsonp, trigger,
 
   // to extract callback name from URL
-  JSONP = /(?:callback|jsonp)=([^\x26]*)/;
+  JSONP = /(?:callback|jsonp)=([^\x26]+?)/; // [ag] empty callbacks are not welcome
+                                            // also make it less greedy ?
 
   /* ========== VAR BLOCK END ========== */
 
@@ -27,14 +28,16 @@ function loadJSON(url, callback) {
       event.initEvent('onpage', true, true);
       frameElement.dispatchEvent(event);
     } :
-    function(event) {
-      event = document.createEventObject();
-      frameElement.fireEvent('onpage', event);
+    function() { // [ag] in IE event is global, no need to shadow it
+      frameElement.fireEvent(
+        'onpage',
+        document.createEventObject()
+      );
     };
 
   // initialize JSONP wrapper
   if (typeof url == 'string' &&
-    (jsonp = url.match(JSONP)) && jsonp[1]) {
+    (jsonp = url.match(JSONP))) { // [ag] now if match is truthy ;-)
 
     // build html string to inject
     html = '<script>' +
@@ -46,8 +49,10 @@ function loadJSON(url, callback) {
         'var d = document,' +
         'r = d.documentElement,' +
         's = d.createElement("script");' +
-        'r.insertBefore(s, r.firstChild);' +
+        's.onerror = ' + jsonp[1] + ';' + // [ag] you really want this notification too!
+        's.type = "text/javascript";' +   // [ag] IIRC some old IE won't script without it
         's.src = "' + url + '";' +
+        'r.insertBefore(s, r.firstChild);' +
       '};' +
     '</script>';
 
@@ -59,6 +64,12 @@ function loadJSON(url, callback) {
 
     // inject html into it
     iframe.src = 'javascript:"' + html + '"';
+
+    // [ag] nitpick:
+    // you can pass a function directly
+    // not sure why you went for the HTML + script injection
+    // javacript:(function(){}());
+    // would/should do it too?
 
     // insert iframe element
     r.insertBefore(iframe, r.firstChild);
@@ -82,5 +93,6 @@ function loadJSON(url, callback) {
       });
     }
   }
+  // [ag] so that if window._123() returns an error you can handle that case too
 
 }
